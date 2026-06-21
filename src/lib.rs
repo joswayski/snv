d4d9d79996a26979c4e0d37925f57302c12a5cce
Jose Valerio <contact@josevalerio.com>
2026-06-21 02:11:55 -0400

    saving

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::{
     os::unix::ffi::OsStrExt,
 };
 
-use sjl::{Logger, json};
+use sjl::{LogLevel::Info, Logger, json};
 
 pub enum SnvErrors {
     FileLoadError,
@@ -34,8 +34,6 @@ pub fn load(file_name: Option<&str>) -> Result<(), SnvErrors> {
     for (index, line) in reader.lines().enumerate() {
         match line {
             Ok(line) => {
-                logger.info(format!("{line}"), ());
-
                 let Some((key, value)) = line.split_once("=") else {
                     logger.warn(
                         format!("Unable to parse line number {} with value: '{}'. Did not find a '=' delimiter, make sure you include it like 'key=value'", index + 1, line), ()
@@ -44,14 +42,23 @@ pub fn load(file_name: Option<&str>) -> Result<(), SnvErrors> {
                 };
 
                 let normalized_key = key.trim();
-                let normalized_value = value.trim();
+                let mut normalized_value = value.trim();
 
+                // Remove wrapper quotes
+                if let Some(stripped_value) = normalized_value
+                    .strip_prefix('"')
+                    .and_then(|v| v.strip_suffix('"'))
+                {
+                    normalized_value = stripped_value
+                }
+
+                logger.info("ENV LINE", json!({normalized_key: normalized_value}));
                 unsafe {
                     std::env::set_var(
                         OsStr::from_bytes(normalized_key.as_bytes()),
                         OsStr::from_bytes(normalized_value.as_bytes()),
                     );
-                };
+                }
             }
             Err(err) => {
                 logger.error(
